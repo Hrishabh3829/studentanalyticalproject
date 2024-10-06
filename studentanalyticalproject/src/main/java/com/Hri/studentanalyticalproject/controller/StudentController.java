958d8a3554ef24e24d21344cfedf26f4615e1f39
Hrishabh3829 <2200032748@kluniversity.in>
2024-10-06 23:18:39 +0530

    "adding data in database"

--- studentanalyticalproject/src/main/java/com/Hri/studentanalyticalproject/controller/StudentController.java
+++ studentanalyticalproject/src/main/java/com/Hri/studentanalyticalproject/controller/StudentController.java
@@ -1,4 +1,25 @@
 package com.Hri.studentanalyticalproject.controller;
 
+import com.Hri.studentanalyticalproject.model.Student;
+import com.Hri.studentanalyticalproject.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
 public class StudentController {
-}
+
+    @Autowired
+    private StudentService studentService;
+
+
+        @PostMapping("/add")
+        public String add(@RequestBody Student student){
+            studentService.saveStudent(student);
+            return "New student added";
+        }
+
+    }
--- studentanalyticalproject/src/main/java/com/Hri/studentanalyticalproject/service/StudentService.java
+++ studentanalyticalproject/src/main/java/com/Hri/studentanalyticalproject/service/StudentService.java
@@ -1,4 +1,7 @@
 package com.Hri.studentanalyticalproject.service;
 
+import com.Hri.studentanalyticalproject.model.Student;
+
 public interface StudentService {
+   public Student saveStudent(Student student);
 }
